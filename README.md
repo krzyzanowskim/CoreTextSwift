0682f40ace1b0bc5b8683f164248b2cd70ce36c1
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2020-06-14 00:19:30 +0200

    add example

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # CoreTextSwift
 
 Swifty CoreText API
+
+example:
+
+```swift
+let attributedString = NSAttributedString(string: "abcdefgh")
+for run in attributedString.line().glyphRuns() {
+  let font = run.font
+  for glyph in run.glyphs() {
+    let glyphPath = font.path(for: glyph)
+    // draw path
+  }
+}
+```
--- Sources/CoreTextSwift/CFAttributedString+CoreText.swift
+++ Sources/CoreTextSwift/CFAttributedString+CoreText.swift
@@ -1,15 +1,36 @@
 import CoreText
 
-extension CFAttributedString {
+public extension CFAttributedString {
 
   /// Creates a single immutable line object directly from an attributed string.
-  public func line() -> CTLine {
+  func line() -> CTLine {
     CTLineCreateWithAttributedString(self)
   }
 
   /// Creates an immutable framesetter object from an attributed string.
-  public func framesetter() -> CTFramesetter {
+  func framesetter() -> CTFramesetter {
     CTFramesetterCreateWithAttributedString(self)
   }
 
 }
+
+
+#if canImport(UIKit)
+import UIKit
+#endif
+
+#if canImport(AppKit)
+import AppKit
+#endif
+
+#if canImport(UIKit) || canImport(AppKit)
+public extension NSAttributedString {
+  func line() -> CTLine {
+    (self as CFAttributedString).line()
+  }
+
+  func framesetter() -> CTFramesetter {
+    (self as CFAttributedString).framesetter()
+  }
+}
+#endif
